340fc9f4c685e6e2a234adc3d483143753573971
Alejo Boga <alejoboga20@gmail.com>
2022-04-11 16:22:14 -0300

    Use GetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
+import { GetStaticProps } from 'next';
 import { Layout } from '../components/layouts';
 
-const HomePage = () => {
+const HomePage = (props: any) => {
+	console.log(props);
 	return (
 		<Layout title='Pokemon List'>
 			<h1>Hello Next</h1>
+
+			<ul>
+				<li>Pokemon</li>
+				<li>Pokemon</li>
+				<li>Pokemon</li>
+				<li>Pokemon</li>
+				<li>Pokemon</li>
+			</ul>
 		</Layout>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+	return {
+		props: {},
+	};
 };
 
 export default HomePage;
