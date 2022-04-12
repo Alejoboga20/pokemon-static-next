df834896eb4f6312a9c8e58db5d8a9b2b5fa86e9
Alejo Boga <alejoboga20@gmail.com>
2022-04-12 16:56:58 -0300

    Create navigation

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react';
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react';
 import Image from 'next/image';
+import NextLink from 'next/link';
 
 export const Navbar = () => {
 	const { theme } = useTheme();
@@ -22,15 +23,23 @@ export const Navbar = () => {
 				width={70}
 				height={70}
 			/>
-			<Text color='white' h2>
-				P
-			</Text>
-			<Text color='white' h3>
-				okemon
-			</Text>
+			<NextLink href='/' passHref>
+				<Link>
+					<Text color='white' h2>
+						P
+					</Text>
+					<Text color='white' h3>
+						okemon
+					</Text>
+				</Link>
+			</NextLink>
 
 			<Spacer css={{ flex: 1 }} />
-			<Text color='white'>Favorites</Text>
+			<NextLink href='/favorites' passHref>
+				<Link>
+					<Text color='white'>Favorites</Text>
+				</Link>
+			</NextLink>
 		</div>
 	);
 };
