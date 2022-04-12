40f39ecd585985e5836f0351243bcdd45cb17e6b
Alejo Boga <alejoboga20@gmail.com>
2022-04-12 16:26:07 -0300

    Use Static paths

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
+import { PokemonDetails, SmallPokemon } from '../../interfaces/pokemon';
+import { pokeApi } from '../../api';
 import { Layout } from '../../components/layouts';
-import { SmallPokemon } from '../../interfaces';
 
-const PokemonPage = () => {
+const PokemonPage: NextPage<PokemonPageProps> = ({ pokemon }: PokemonPageProps) => {
 	return (
 		<Layout>
-			<h1>Hello</h1>
+			<h1>{pokemon.name}</h1>
 		</Layout>
 	);
 };
@@ -13,5 +14,27 @@ const PokemonPage = () => {
 interface PokemonPageProps {
 	pokemon: SmallPokemon;
 }
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+	const pokemonList = [...Array(151)].map((value, index) => `${index + 1}`);
+
+	return {
+		paths: pokemonList.map((id) => ({
+			params: { id },
+		})),
+		fallback: false,
+	};
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const { id } = params as { id: string };
+	const { data } = await pokeApi.get<PokemonDetails>(`/pokemon/${id}`);
+
+	return {
+		props: {
+			pokemon: data,
+		},
+	};
+};
 
 export default PokemonPage;
