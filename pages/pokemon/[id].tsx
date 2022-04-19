4cf5d1f8dc150518dc8c58cfe37ab1851c6adb1e
Alejo Boga <alejoboga20@gmail.com>
2022-04-19 09:55:25 -0300

    Use ISG

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -99,15 +99,26 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 		paths: pokemonList.map((id) => ({
 			params: { id },
 		})),
-		fallback: false,
+		fallback: 'blocking',
 	};
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	const { id } = params as { id: string };
 
+	const pokemon = await getPokemonInfo(id);
+
+	if (!pokemon) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
 	return {
-		props: { pokemon: await getPokemonInfo(id) },
+		props: { pokemon },
 		revalidate: 86400, //in seconds
 	};
 };
